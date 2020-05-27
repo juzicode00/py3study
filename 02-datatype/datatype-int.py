2deffabc9b17b163d7fab1fb3445f2f36ffc9b93
juzicode00 <juzicode00@163.com>
2020-05-28 00:50:34 +0800

    增加datatype int 和float

---
+++ 02-datatype/datatype-int.py
@@ -0,0 +1,23 @@
+'''
+author: juzicode
+address: www.juzicode.com
+wechat: juzicode
+date: 2020.5.27
+'''
+print('\n-----欢迎来到www.juzicode.com!-----')
+print('int数据类型例程：')
+a=5
+b=3
+print('a =',a)
+print('b =',b)
+print('a类型：',type(a))
+print('b类型：',type(b))
+print('a+b =',a+b)
+print('a-b =',a-b)
+print('a*b =',a*b)
+print('a//b =',a//b)
+print('a%b =',a%b)
+print('a/b =',a/b)
+
+print('a//b的数据类型：',type(a//b))
+print('a/b的数据类型：',type(a/b))
